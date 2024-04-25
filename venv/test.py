01fee23289f26eab0f6ee06e17de5f9c1542f5b4
BeardyDev <58749047+Treblepickster@users.noreply.github.com>
2024-04-25 22:25:23 +0100

    another useless change

--- venv/test.py
+++ venv/test.py
@@ -1,2 +1,32 @@
-hi = 'heloo'
+class Gen():
+    current = 0
+    def __init__(self, first, last):
+        self.first = first
+        self.last = last
+    
+    def __iter__(self):
+        return self
+    
+    def __next__(self):
+        if Gen.current < self.last:
+            num = Gen.current
+            Gen.current += 1
+            return num
 
+
+def fib(num):
+    current = 0
+    l_num = 0
+    for x in range(num):
+        if x == 0:
+            print(f'Fibanacci[{x}] is 0')
+        elif x == 1:
+            current = x
+            print(f'Fibanacci[{x}] is 1')
+        else:
+            new = l_num + current
+            print(f'Fibanacci[{x}] is {new}')
+            l_num = current
+            current = new 
+
+fib(21)
